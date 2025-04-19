4304b82121149d75ed197b3f4cc4e8e5425b8a16
fullstackjavadeveloperpune <fullstackjavadeveloperpune@gmail.com>
2025-04-19 21:23:00 +0530

    added comments

--- Welcome.java
+++ Welcome.java
@@ -1,7 +1,8 @@
 public class Welcome{
 
+	// this is main method
 	public static void main(String args[]){
-	
+		// added by Kiran Jadhav
 		System.out.println("WELCOME TO FULL STACK JAVA DEVELOPER, PUNE");
 	}
 }
